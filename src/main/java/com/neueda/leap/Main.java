0e9cf1b14fb963c2d9df3959da055feadd04d78b
Sam Adams <sammyadams04@gmail.com>
2026-08-26 16:48:24 +0000

    add my name in main java println for Jenkins pipeline auto build testing using polling

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,8 +2,9 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello world from the team's Sprint 1 project skeleton");
+        System.out.println("Hello world from the Team Rocket's Sprint 1 project skeleton");
         System.out.println("Container is up. Sleeping so you can inspect it.");
+        System.out.println("- Sam Adams.");
         Thread.sleep(600_000);
     }
 }
